e4551118d7f0590573418624812d0cd466d5e6c0
pickyko <68360714+pickyko@users.noreply.github.com>
2022-12-07 23:06:51 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Location of first config to compare
-f1_name = "config.cfg"
+f1_name = "exampleautoexec1.cfg"
 
 # Location of second config to compare
-f2_name = "autoexec.cfg"
+f2_name = "exampleautoexec2.cfg"
 
 # Will ignore lines that start items in this list
 # Bind and alias have to be in there because they have 2 values
